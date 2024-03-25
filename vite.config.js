9a354114bff62ce9db9015bf0a3ea41277a10e05
billxu <billxu0521@gmail.com>
2024-03-26 00:01:58 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,8 @@ export default defineConfig({
   build: {
     minify: true,
   },
+  base: process.env.NODE_ENV === 'production' ? '/FJU_liff/' : './',
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
