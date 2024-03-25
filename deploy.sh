afd0efe524baadffba1435c6805969ce7fc011fa
billxu <billxu0521@gmail.com>
2024-03-26 00:42:06 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ npm run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
-echo 'https://fjuh.one' > CNAME
+echo 'fjuh.one' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
