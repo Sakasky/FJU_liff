f092f4da06e6d5092f42de97f53d1517ec1fbbab
billxu <billxu0521@gmail.com>
2024-03-26 17:43:38 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import './assets/css/main.css'
 import router from "./router/index";
 import { createPinia } from "pinia";
+import VConsole from 'vconsole';
 
 const app = createApp(App);
 const pinia = createPinia();
+const vConsole = new VConsole();
 
 app.use(pinia);
 app.use(router);
